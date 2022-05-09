9a59d4b2f3f6c3579ca0485d20e3fbc757e59060
zhangyf <zhangyf@phxg.cn>
2022-05-09 13:03:17 +0800

    mod: update

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'workflow', // 工作流改进
+        'wip', // 开发中
+        'mod', // 不确定分类的修改
+      ],
+    ],
+  },
 }
--- mock/role/routes.js
+++ mock/role/routes.js
@@ -231,17 +231,6 @@ const asyncRoutes = [
     ],
   },
 
-  {
-    path: '/external-link',
-    component: 'layout/Layout',
-    children: [
-      {
-        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
-        meta: { title: 'External Link', icon: 'link' },
-      },
-    ],
-  },
-
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true },
 ]
