036b0a9e15ffa8f2c78f7966f4678e27920b8c26
zhangyf <feng19105306350@163.com>
2022-05-12 12:44:51 +0800

    chore: vitest

---
+++ src/test/basic.test.js
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest'
+// 如果要使用别名 需要新建vite.config.js 配置alias
+import { capitalize, toHump, toLine } from '../utils/letter'
+
+describe('utils', () => {
+  it('toLine', () => {
+    expect(toLine('camelCase')).toEqual('camel-case')
+  })
+  it('toLine', () => {
+    expect(toLine('CamelCase')).toEqual('camel-case')
+  })
+  it('toHump', () => {
+    expect(toHump('camel-case')).toEqual('camelCase')
+  })
+  it('firstUpper', () => {
+    expect(capitalize('firstUpper')).toEqual('FirstUpper')
+  })
+})
