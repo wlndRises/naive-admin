4312d63a35ebbb3ba776b3140fd2ac305a865cfa
zhangyf <feng19105306350@163.com>
2022-06-17 21:06:11 +0800

    refactor: update

--- src/utils/implementation.js
+++ src/utils/implementation.js
@@ -1,10 +1,10 @@
 /*
- * @Description: 手写实现lodash的实用函数
+ * @Description: 实用函数
  * @Version: 1.0
  * @Autor: Wind
  * @Date: 2022-04-30 11:56:48
  * @LastEditors: Wind
- * @LastEditTime: 2022-06-16 11:17:53
+ * @LastEditTime: 2022-06-17 21:06:54
  */
 import { isDate, isJson, isObject, isArray, isDef } from './is'
 
@@ -95,4 +95,55 @@ export function merge(target) {
   }
 
   return target
+}
+
+export function softBind(fn, obj) {
+  const curried = Array.prototype.slice.call(arguments, 2)
+  const bound = function () {
+    return fn.apply(
+      !this || this === (window || global) ? obj : this,
+      Array.prototype.concat.apply(curried, arguments)
+    )
+  }
+  bound.prototype = Object.create(fn.prototype)
+  return bound
+}
+
+/**
+ * @description: 纵向合并单元格
+ * @param {Array} 表格数据
+ * @param {Array | String} 需要纵向合并列 依据的字段组成的数组 也可传单个字段字符串
+ * @return {Array} 每行合并的格数组成的数组
+ * @author: Wind
+ * eg:
+ * const row = rowSpanList[rowIndex]
+ * const col = _row > 0 ? 1 : 0
+ * return [row, col]
+ */
+export function getRowSpanList(data, contentList) {
+  // 单个字段转为数组
+  !Array.isArray(contentList) && (contentList = [contentList])
+  // 每次都清空之前存储的 保证遍历的数据是最新的数据。以免造成数据渲染混乱
+  const rowSpanList = []
+  let pos = 0
+  // 遍历数据
+  data.forEach((_, index) => {
+    // 判断是否是第一项
+    if (index === 0) {
+      rowSpanList.push(1)
+      pos = 0
+    } else {
+      // 不是第一项时，就根据标识去存储
+      if (contentList.every(content => data[index][content] === data[index - 1][content])) {
+        // 查找到符合条件的数据时每次要把之前存储的数据+1
+        rowSpanList[pos] += 1
+        rowSpanList.push(0)
+      } else {
+        // 没有符合的数据时，要记住当前的index
+        rowSpanList.push(1)
+        pos = index
+      }
+    }
+  })
+  return rowSpanList
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,21 +2,9 @@
 import { omitBy } from 'lodash-es'
 import { isEmpty, isArray, isObject, isString } from '@/utils/is'
 
-export const filterEmpty = (v, fn = isEmpty) => {
-  if (isArray(v)) return v.filter(fn)
-  if (isObject(v)) return omitBy(v, fn)
-}
-
-export function softBind(fn, obj) {
-  const curried = Array.prototype.slice.call(arguments, 2)
-  const bound = function () {
-    return fn.apply(
-      !this || this === (window || global) ? obj : this,
-      Array.prototype.concat.apply(curried, arguments)
-    )
-  }
-  bound.prototype = Object.create(fn.prototype)
-  return bound
+export const filterEmpty = (val, fn = isEmpty) => {
+  if (isArray(val)) return val.filter(fn)
+  if (isObject(val)) return omitBy(val, fn)
 }
 
 export function cloneFunc(func) {
@@ -37,17 +25,6 @@ export function reverse(source) {
   }
 }
 
-/**
- * @description: 错误处理
- * eg:
- * let [err, res] = await errorCapture(asyncFunc)
- * if (err) {
- *   ... capture the error
- * }
- * 如果你想在出现错误时什么也不做 那么你可以
- * const res = await asyncFunc().catch(noop)
- * if (!res) return
- */
 export const errorCapture = async asyncFunc => {
   try {
     const res = await asyncFunc()
@@ -55,43 +32,4 @@ export const errorCapture = async asyncFunc => {
   } catch (err) {
     return [err, null]
   }
-}
-
-/**
- * @description: 纵向合并单元格
- * @param {Array} 表格数据
- * @param {Array | String} 需要纵向合并列 依据的字段组成的数组 也可传单个字段字符串
- * @return {Array} 每行合并的格数组成的数组
- * @author: Wind
- * eg:
- * const row = rowSpanList[rowIndex]
- * const col = _row > 0 ? 1 : 0
- * return [row, col]
- */
-export function getRowSpanList(data, contentList) {
-  // 单个字段转为数组
-  !Array.isArray(contentList) && (contentList = [contentList])
-  // 每次都清空之前存储的 保证遍历的数据是最新的数据。以免造成数据渲染混乱
-  const rowSpanList = []
-  let pos = 0
-  // 遍历数据
-  data.forEach((_, index) => {
-    // 判断是否是第一项
-    if (index === 0) {
-      rowSpanList.push(1)
-      pos = 0
-    } else {
-      // 不是第一项时，就根据标识去存储
-      if (contentList.every(content => data[index][content] === data[index - 1][content])) {
-        // 查找到符合条件的数据时每次要把之前存储的数据+1
-        rowSpanList[pos] += 1
-        rowSpanList.push(0)
-      } else {
-        // 没有符合的数据时，要记住当前的index
-        rowSpanList.push(1)
-        pos = index
-      }
-    }
-  })
-  return rowSpanList
 }
